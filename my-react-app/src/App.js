8cbc9e549c392f7f7b701cd4be7cc42b6d01076d
Rohit <rohitsaste45@gmail.com>
2023-06-15 14:56:11 +0530

    react

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,29 +1,28 @@
 import { useState } from "react";
 
 
-function App(){
+function App() {
 
   return (
     <>
-      <h1>Props Demo</h1>
-      <ListDemo/>
+      <h1>Stateful List</h1>
+      <ListDemo />
 
     </>
   )
 }
 
-function ListDemo(){
-  let [list,setList] = useState(["Pune"]);
+function ListDemo() {
+  let [list, setList] = useState(["Pune"]);
 
-  let addItemAction = () =>{
-    let newList =[...list, "Mumbai"];
-    console.log(newList);
+  let addItemAction = () => {
+    let newList = [...list, "Mumbai"];
     setList(newList);
   };
   return (
     <>
 
-    <input type="button" value="Add New Item" onClick={addItemAction} />
+      <input type="button" value="Add New Item" onClick={addItemAction} />
       {list.map((item) => (
         <h1>Hello {item}</h1>
       ))}
